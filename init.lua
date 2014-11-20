2bcb2a10d11a4e1ff6ea00329abb80ba3b754785
Megaf <mmegaf@gmail.com>
2014-11-20 18:50:20 +0000

    Added print('[suicidepill] loaded.')
    And removed not used code

--- init.lua
+++ init.lua
@@ -1,23 +1,11 @@
 minetest.register_node("suicidepill:suicidepill",{
   description = "A suicide pill",
---	drawtype = "plantlike",
 	sunlight_propagates = true,
 --	visual_scale = 1.0,
 	tiles = {"blackpill.png"},
---	inventory_image = "mushroom_red.png",
---	wield_image = "mushroom_red.png",
 	groups = {oddly_breakable_by_hand=1,attached_node=1},
 	paramtype = "light",
 	on_use = minetest.item_eat(-20),
---[[	selection_box = {
-		type = "fixed",
-		fixed = {-3, -1.5, -3, 3, 5, 3}
-	}, ]]
-})
+	})
 
---[[
-minetest.register_craft( {
-  type = "shapeless",
-  output = ""suicidepill:suicidepill",
-  recipe = { "mushroom:red" , "default:mese_crystal" },
-}) ]]
+print('[suicidepill] loaded.')
